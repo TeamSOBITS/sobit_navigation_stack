59e4519d7c378d543863d039b4a3d985a48266b7
Daiki Murakami <dai.murakami98@gmail.com>
2021-08-24 16:03:25 +0900

    add : save_3dmap_command

--- sobit_mapping/script/save_3dmap_command.py
+++ sobit_mapping/script/save_3dmap_command.py
@@ -11,25 +11,28 @@ Enter_key_trig = str(raw_input("\n\nWhen you save this map, press the 'Enter' ke
 
 map_dir_path = rospy.get_param('/save_3dmap_command/map_save_path')
 map_name_3d = "map_3d_" + str(datetime.datetime.today().month) + "_" + str(datetime.datetime.today().day) + "_" + str(datetime.datetime.today().hour) + "_" + str(datetime.datetime.today().minute) + ".bt"
-
 cmd_3d = "rosrun octomap_server octomap_saver -f " + map_dir_path + map_name_3d
 
-map_name_2d = "map_2d_" + str(datetime.datetime.today().month) + "_" + str(datetime.datetime.today().day) + "_" + str(datetime.datetime.today().hour) + "_" + str(datetime.datetime.today().minute)
+map_name_2d_multi = "map_2d_multi_" + str(datetime.datetime.today().month) + "_" + str(datetime.datetime.today().day) + "_" + str(datetime.datetime.today().hour) + "_" + str(datetime.datetime.today().minute) + ".bt"
+cmd_3d_tmp = "rosrun octomap_server octomap_saver -f " + map_dir_path + map_name_2d_multi 
+cmd_2d_multi = "rosrun octomap_server octomap_server_multilayer " + map_dir_path + map_name_2d_multi
 
+map_name_2d = "map_2d_" + str(datetime.datetime.today().month) + "_" + str(datetime.datetime.today().day) + "_" + str(datetime.datetime.today().hour) + "_" + str(datetime.datetime.today().minute)
 cmd_2d = "rosrun map_server map_saver -f " + map_dir_path + map_name_2d
 
 print "\n\n" + map_dir_path + map_name_3d
 print "\n\nPlease wait...\n\n"
-
 subprocess.call(cmd_3d, shell=True)# Do the command
-print "\n\n" + map_dir_path + map_name_3d
+
+print "\n\n" + map_dir_path + cmd_2d_multi
+print "\n\nPlease wait...\n\n"
+subprocess.call(cmd_3d_tmp, shell=True)# Do the command
+subprocess.call(cmd_2d_multi, shell=True)# Do the command
 
 print "\n\n" + map_dir_path + map_name_2d
 print "\n\nPlease wait...\n\n"
-
 subprocess.call(cmd_2d, shell=True)# Do the command
 
-print "\n\n" + map_dir_path + map_name_2d
 print "\n\nOK.finished. Please check it out!"
 
 print "\n\n 閉じるときは❌ボタンで。"
