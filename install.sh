29f2cbb4f496a121eff62724c2293c346c83e117
Daiki Murakami <dai.murakami98@gmail.com>
2021-08-24 14:32:25 +0900

    add : sobit_turtlebot_octomap

--- install.sh
+++ install.sh
@@ -1,66 +1,84 @@
 #!/bin/sh
 
 sudo apt update 
+# Install gmapping, amcl, move_base etc.
 # Ref : https://demikko-no-bibouroku.hatenablog.com/entry/2020/08/11/015340
-echo "Install ros-melodic-joy"
+echo -e "\e[34m\nInstall ros-melodic-joy\e[m"
 sudo apt-get install ros-melodic-joy -y
 
-echo "Install ros-melodic-teleop-twist-joy"
+echo -e "\e[34m\nInstall ros-melodic-teleop-twist-joy\e[m"
 sudo apt-get install ros-melodic-teleop-twist-joy -y
 
-echo "Install ros-melodic-teleop-twist-keyboard"
+echo -e "\e[34m\nInstall ros-melodic-teleop-twist-keyboard\e[m"
 sudo apt-get install ros-melodic-teleop-twist-keyboard -y
 
-echo "Install ros-melodic-laser-proc"
+echo -e "\e[34m\nInstall ros-melodic-laser-proc\e[m"
 sudo apt-get install ros-melodic-laser-proc -y
 
-echo "Install ros-melodic-rgbd-launch"
+echo -e "\e[34m\nInstall ros-melodic-rgbd-launch\e[m"
 sudo apt-get install ros-melodic-rgbd-launch -y
 
-echo "Install ros-melodic-depthimage-to-laserscan"
+echo -e "\e[34m\nInstall ros-melodic-depthimage-to-laserscan\e[m"
 sudo apt-get install ros-melodic-depthimage-to-laserscan -y
 
-echo "Install ros-melodic-rosserial-arduino"
+echo -e "\e[34m\nInstall ros-melodic-rosserial-arduino\e[m"
 sudo apt-get install ros-melodic-rosserial-arduino -y
 
-echo "Install ros-melodic-rosserial-python"
+echo -e "\e[34m\nInstall ros-melodic-rosserial-python\e[m"
 sudo apt-get install ros-melodic-rosserial-python -y
 
-echo "Install ros-melodic-rosserial-server"
+echo -e "\e[34m\nInstall ros-melodic-rosserial-server\e[m"
 sudo apt-get install ros-melodic-rosserial-server -y
 
-echo "Install ros-melodic-rosserial-client"
+echo -e "\e[34m\nInstall ros-melodic-rosserial-client\e[m"
 sudo apt-get install ros-melodic-rosserial-client -y
 
-echo "Install ros-melodic-rosserial-msgs"
+echo -e "\e[34m\nInstall ros-melodic-rosserial-msgs\e[m"
 sudo apt-get install ros-melodic-rosserial-msgs -y
 
-echo "Install ros-melodic-amcl"
+echo -e "\e[34m\nInstall ros-melodic-amcl\e[m"
 sudo apt-get install ros-melodic-amcl -y
 
-echo "Install ros-melodic-map-server"
+echo -e "\e[34m\nInstall ros-melodic-map-server\e[m"
 sudo apt-get install ros-melodic-map-server -y
 
-echo "Install ros-melodic-move-base"
+echo -e "\e[34m\nInstall ros-melodic-move-base\e[m"
 sudo apt-get install ros-melodic-move-base -y
 
-echo "Install ros-melodic-urdf"
+echo -e "\e[34m\nInstall ros-melodic-urdf\e[m"
 sudo apt-get install ros-melodic-urdf -y
 
-echo "Install ros-melodic-xacro"
+echo -e "\e[34m\nInstall ros-melodic-xacro\e[m"
 sudo apt-get install ros-melodic-xacro -y
 
-echo "Install ros-melodic-compressed-image-transport"
+echo -e "\e[34m\nInstall ros-melodic-compressed-image-transport\e[m"
 sudo apt-get install ros-melodic-compressed-image-transport -y
 
-echo "Install ros-melodic-rqt-image-view "
+echo -e "\e[34m\nInstall ros-melodic-rqt-image-view\e[m"
 sudo apt-get install ros-melodic-rqt-image-view -y
 
-echo "Install ros-melodic-gmapping"
+echo -e "\e[34m\nInstall ros-melodic-gmapping\e[m"
 sudo apt-get install ros-melodic-gmapping -y
 
-echo "Install ros-melodic-navigation"
+echo -e "\e[34m\nInstall ros-melodic-navigation\e[m"
 sudo apt-get install ros-melodic-navigation -y
 
-echo "Install ros-melodic-interactive-markers"
+echo -e "\e[34m\nInstall ros-melodic-interactive-markers\e[m"
 sudo apt-get install ros-melodic-interactive-markers -y
+
+# Install rtabmap, octomap etc.
+# Ref : https://qiita.com/ryu_software/items/d13a70aacfc6a71cacdb#%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB
+echo -e "\e[34m\nInstall os-melodic-octomap\e[m"
+sudo apt-get install ros-melodic-octomap -y
+
+echo -e "\e[34m\nInstall os-melodic-octomap-mapping\e[m"
+sudo apt-get install ros-melodic-octomap-mapping -y
+
+echo -e "\e[34m\nInstall ros-melodic-octomap-rviz-plugins\e[m"
+sudo apt-get install ros-melodic-octomap-rviz-plugins -y
+
+echo -e "\e[34m\nInstall ros-melodic-rtabmap\e[m"
+sudo apt-get install ros-melodic-rtabmap -y
+
+echo -e "\e[34m\nInstall ros-melodic-rtabmap-ros\e[m"
+sudo apt install ros-melodic-rtabmap-ros -y
--- sobit_mapping/README.md
+++ sobit_mapping/README.md
@@ -2,7 +2,7 @@
 - 地図生成パッケージ
 
 ## sobit_turtlebot_gmapping.launch
-SOBIT EDU, MINI用の地図生成(gmapping)
+SOBIT EDU, MINI用の2次元地図生成(gmapping)
 
 ```bash
 $ roslaunch sobit_mapping sobit_turtlebot_gmapping.launch 
@@ -56,6 +56,20 @@ $ roslaunch sobit_mapping sobit_turtlebot_create_location_file.launch
 - [sobit_turtlebot_create_location_file.launch](/sobit_mapping/launch/sobit_turtlebot_create_location_file.launch)
 <div align="center">
     <img src="doc/img/sobit_turtlebot_create_location_file.png">
+</div> 
+
+## sobit_turtlebot_octomap
+SOBIT EDU, MINI用の3次元地図生成(octomap)
+
+```bash
+roslaunch sobit_mapping sobit_turtlebot_octomap.launch 
+```
+- [sobit_turtlebot_octomap.launch](/sobit_mapping/launch/sobit_turtlebot_octomap.launch)
+- [Setup RTAB-Map on Your Robot!](http://wiki.ros.org/rtabmap_ros/Tutorials/SetupOnYourRobot)
+- [octomap_server](http://wiki.ros.org/octomap_server)
+
+<div align="center">
+    <img src="doc/img/sobit_turtlebot_octomap.png">
 </div> 
 
 ---
