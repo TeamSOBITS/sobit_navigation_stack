4c7abab0db75acf262d58ccee732ba60dc1d037c
OnoFumiya <fumiyaono.choi@gmail.com>
2023-11-15 19:45:37 +0900

    develop oss

--- install.sh
+++ install.sh
@@ -2,50 +2,6 @@
 # # 参考①：https://demikko-no-bibouroku.hatenablog.com/entry/2020/08/11/015340
 # # 参考②：https://qiita.com/ryu_software/items/d13a70aacfc6a71cacdb#%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB
 
-
-# echo "╔══╣ Install: Sobit Navigation Stack (STARTING) ╠══╗"
-
-
-# sudo apt-get update
-
-# # Install gmapping, amcl, move_base etc.
-# sudo apt-get install -y \
-#     ros-${ROS_DISTRO}-joy \
-#     ros-${ROS_DISTRO}-teleop-twist-joy \
-#     ros-${ROS_DISTRO}-teleop-twist-keyboard \
-#     ros-${ROS_DISTRO}-laser-proc \
-#     ros-${ROS_DISTRO}-rgbd-launch \
-#     ros-${ROS_DISTRO}-depthimage-to-laserscan \
-#     ros-${ROS_DISTRO}-rosserial-arduino \
-#     ros-${ROS_DISTRO}-rosserial-python \
-#     ros-${ROS_DISTRO}-rosserial-server \
-#     ros-${ROS_DISTRO}-rosserial-client \
-#     ros-${ROS_DISTRO}-rosserial-msgs \
-#     ros-${ROS_DISTRO}-amcl \
-#     ros-${ROS_DISTRO}-map-server \
-#     ros-${ROS_DISTRO}-move-base \
-#     ros-${ROS_DISTRO}-urdf \
-#     ros-${ROS_DISTRO}-xacro \
-#     ros-${ROS_DISTRO}-compressed-image-transport \
-#     ros-${ROS_DISTRO}-rqt-image-view \
-#     ros-${ROS_DISTRO}-gmapping \
-#     ros-${ROS_DISTRO}-navigation \
-#     ros-${ROS_DISTRO}-interactive-markers
-
-# # Install rtabmap, octomap etc.
-# sudo apt-get install -y \
-#     ros-${ROS_DISTRO}-octomap \
-#     ros-${ROS_DISTRO}-octomap-mapping \
-#     ros-${ROS_DISTRO}-octomap-rviz-plugins \
-#     ros-${ROS_DISTRO}-rtabmap \
-#     ros-${ROS_DISTRO}-rtabmap-ros \
-#     ros-${ROS_DISTRO}-pointcloud-to-laserscan \
-#     ros-${ROS_DISTRO}-ira-laser-tools
-
-
-# echo "╚══╣ Install: Sobit Navigation Stack (FINISHED) ╠══╝"
-
-
 echo "╔══╣ Install: Sobit Navigation Stack (STARTING) ╠══╗"
 
 sudo apt-get update
@@ -54,30 +10,21 @@ cd ~/catkin_ws/src/sobit_navigation_stack/
 mkdir sobit_navigation_packages
 cd ~/catkin_ws/src/sobit_navigation_stack/sobit_navigation_packages/
 if [ "clone_mode" == $1 ]; then
+    echo -e "\e[34m git clone mode \e[m"
     git clone https://github.com/ros-perception/slam_gmapping.git
     git clone https://github.com/ros-perception/openslam_gmapping.git
     git clone https://github.com/ros-planning/navigation_msgs.git
     git clone https://github.com/ros-planning/navigation.git
     git clone https://github.com/ros/geometry2.git
 
-elif [ "install_mode" == $1 ]; then
-    sudo apt-get install -y \
-        ros-${ROS_DISTRO}-amcl \
-        ros-${ROS_DISTRO}-map-server \
-        ros-${ROS_DISTRO}-move-base \
-        ros-${ROS_DISTRO}-gmapping \
-        ros-${ROS_DISTRO}-navigation
-
 else 
-    echo -e "\e[31m No install.sh. Please chack the README \e[m"
-    ###
+    echo -e "\e[34m install mode \e[m"
     sudo apt-get install -y \
         ros-${ROS_DISTRO}-amcl \
         ros-${ROS_DISTRO}-map-server \
         ros-${ROS_DISTRO}-move-base \
         ros-${ROS_DISTRO}-gmapping \
         ros-${ROS_DISTRO}-navigation
-    ###
 fi
 
 git clone https://github.com/TeamSOBITS/als_ros.git
--- sobit_navigation/scripts/rgb_checker.py
+++ sobit_navigation/scripts/rgb_checker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import rospy
+import yaml
 import csv
 import sys
 import cv2
@@ -9,33 +10,44 @@ import numpy as np
 from PIL import Image
 
 
-def checker(R, G, B):
+def checker():
+    rospy.init_node("rgb_checker")
+    rospack = rospkg.RosPack()
+    package_path = rospack.get_path("sobit_navigation")
+    with open(package_path + "/param/floor_color/rgb_base.yaml", "r") as yml:
+        rgb_base = yaml.safe_load(yml)
+    R = rgb_base["R"]
+    G = rgb_base["G"]
+    B = rgb_base["B"]
     if ((R == None) or (G == None) or (B == None)):
         print("NO SETTING")
         return
-    rospack = rospkg.RosPack()
-    rospack.list()
-    package_path = rospack.get_path("sobit_navigation")
     rgb_array = np.zeros((500,500,3))
     for i in range(500):
         for j in range(500):
-            rgb_array[i][j] = [R, G, B] #########
+            rgb_array[i][j] = [R, G, B]
     out_put = Image.fromarray(rgb_array.astype(np.uint8))
-    out_put.save(package_path + "/param/sobit_pro_multi_sensor/rgb_base.jpg")  #####
-    image = cv2.imread(package_path + "/param/sobit_pro_multi_sensor/rgb_base.jpg")#####
+    out_put.save(package_path + "/param/floor_color/rgb_base.jpg")
+    image = cv2.imread(package_path + "/param/floor_color/rgb_base.jpg")
     cv2.imshow("rgb_base", image)
     cv2.waitKey(0)
+    rospy.spin()
     return
 
-def main():
-    rospy.init_node("rgb_checker")
-    R = rospy.get_param("/rgb_checker/rgb/R", 200)
-    G = rospy.get_param("/rgb_checker/rgb/G", 200)
-    B = rospy.get_param("/rgb_checker/rgb/B", 200)
-    rospy.loginfo("R = {0}, G = {1}, B = {2}".format(R,G,B))
-    checker(R, G, B)
-    return
+# def main():
+    # with open("rgb_base.yaml", "r") as yml:
+    #     config = yaml.safe_load(yml)
+
+
+    # R = rospy.get_param("/rgb_checker/rgb/R", 200)
+    # G = rospy.get_param("/rgb_checker/rgb/G", 200)
+    # B = rospy.get_param("/rgb_checker/rgb/B", 200)
+    # rospy.loginfo("R = {0}, G = {1}, B = {2}".format(R,G,B))
+    # checker(R, G, B)
+    # checker()
+    # return
 
 
 if __name__ == '__main__':
-    main()
+    checker()
+    # main()
