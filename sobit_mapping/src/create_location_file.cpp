6b5ce063f6c8b4af1e86487169c263b1c1f44df7
OnoFumiya <fumiyaono.choi@gmail.com>
2023-11-29 22:29:35 +0900

    develop oss

--- sobit_mapping/src/create_location_file.cpp
+++ sobit_mapping/src/create_location_file.cpp
@@ -12,6 +12,7 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Pose.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Transform.h>
 
@@ -22,72 +23,93 @@ class CreateLocationFile {
 		ros::NodeHandle nh_;
 		ros::NodeHandle pnh_;
 		ros::Publisher pub_pose_;
+        ros::Subscriber sub_msg_;
 		tf2_ros::Buffer tfBuffer_;
 		tf2_ros::TransformListener tf_listener_;
 		std::string file_name_;
-		bool saveLocation( const std::string location_name );
-
+		bool use_robot_;
+		geometry_msgs::Pose nav_goal_msg_;
+		void saveLocation( const std::string location_name );
+		void callbackMessage( const geometry_msgs::PoseStampedConstPtr &msg );
 	public : 
 		CreateLocationFile();
 		void createLocationFile();
 };
 
-bool CreateLocationFile::saveLocation( const std::string location_name ) {
+void CreateLocationFile::saveLocation( const std::string location_name ) {
+	geometry_msgs::Transform transform;
 	try{
-		geometry_msgs::TransformStamped transformStamped;
-		tfBuffer_.canTransform("map","base_footprint", ros::Time(0), ros::Duration(0.5));
-		transformStamped = tfBuffer_.lookupTransform ("map","base_footprint", ros::Time(0));
-
-		geometry_msgs::Transform transform = transformStamped.transform;
-		std::cout << std::endl;
-		std::cout << "point_x   : " << fixed << std::setprecision(7) << transform.translation.x << std::endl;
-		std::cout << "point_y   : " << fixed << std::setprecision(7) << transform.translation.y << std::endl;
-		std::cout << "point_z   : " << fixed << std::setprecision(7) << transform.translation.z << std::endl;
-		std::cout << "rotation_x: " << fixed << std::setprecision(7) << transform.rotation.x << std::endl;
-		std::cout << "rotation_y: " << fixed << std::setprecision(7) << transform.rotation.y << std::endl;
-		std::cout << "rotation_z: " << fixed << std::setprecision(7) << transform.rotation.z << std::endl;
-		std::cout << "rotation_w: " << fixed << std::setprecision(7) << transform.rotation.w << std::endl;
-
-		ofstream ofs(file_name_, ios::app);
-		if(ofs) {
-			ofs << "    - {" <<std::endl;
-			ofs << "        location_name: \"" << location_name << "\","<<std::endl;
-			ofs << "        frame_id: \"map\","<<std::endl;
-			ofs << "        translation_x: " << fixed << std::setprecision(7) << transform.translation.x << "," << std::endl;
-			ofs << "        translation_y: " << fixed << std::setprecision(7) << transform.translation.y << "," << std::endl;
-			ofs << "        translation_z: " << fixed << std::setprecision(7) << transform.translation.z << "," << std::endl;
-			ofs << "        rotation_x: " << fixed << std::setprecision(7) << transform.rotation.x << "," << std::endl;
-			ofs << "        rotation_y: " << fixed << std::setprecision(7) << transform.rotation.y << "," << std::endl;
-			ofs << "        rotation_z: " << fixed << std::setprecision(7) << transform.rotation.z << "," << std::endl;
-			ofs << "        rotation_w: " << fixed << std::setprecision(7) << transform.rotation.w << "," << std::endl;
-			ofs << "    }" <<std::endl;
-			ofs << std::endl;
-			ofs.close();
-			std::cout <<  "「" << file_name_ << "　」として保存完了。"  << std::endl;
-
-			geometry_msgs::PoseStamped pose;
-			pose.pose.position.x = transform.translation.x;
-			pose.pose.position.y = transform.translation.y;
-			pose.pose.position.z = transform.translation.z;
-			pose.pose.orientation.x = transform.rotation.x;
-			pose.pose.orientation.y = transform.rotation.y;
-			pose.pose.orientation.z = transform.rotation.z;
-			pose.pose.orientation.w = transform.rotation.w;
-			pose.header.frame_id = location_name;
-			pub_pose_.publish(pose);
+		if (use_robot_) {
+			geometry_msgs::TransformStamped transformStamped;
+			tfBuffer_.canTransform("map","base_footprint", ros::Time(0), ros::Duration(0.5));
+			transformStamped = tfBuffer_.lookupTransform ("map","base_footprint", ros::Time(0));
+			transform = transformStamped.transform;
 		} else {
-			ofs.close();
-			std::cout <<  file_name_ << "　は作成できませんでした。ｍ(_ _;)ｍ"  << std::endl;
-			std::cout << "ファイルのパスを確認して下さい。" << std::endl;
-		}	
-		return true;
-    } catch( tf2::TransformException &ex ){
+			transform.translation.x = nav_goal_msg_.position.x;
+			transform.translation.y = nav_goal_msg_.position.y;
+			transform.translation.z = nav_goal_msg_.position.z;
+			transform.rotation.x = nav_goal_msg_.orientation.x;
+			transform.rotation.y = nav_goal_msg_.orientation.y;
+			transform.rotation.z = nav_goal_msg_.orientation.z;
+			transform.rotation.w = nav_goal_msg_.orientation.w;
+		}
+	} catch( tf2::TransformException &ex ) {
 		ROS_ERROR("%s", ex.what());
-		return false;
+		return;
 	}
+	std::cout << std::endl;
+	std::cout << "point_x   : " << fixed << std::setprecision(7) << transform.translation.x << std::endl;
+	std::cout << "point_y   : " << fixed << std::setprecision(7) << transform.translation.y << std::endl;
+	std::cout << "point_z   : " << fixed << std::setprecision(7) << transform.translation.z << std::endl;
+	std::cout << "rotation_x: " << fixed << std::setprecision(7) << transform.rotation.x << std::endl;
+	std::cout << "rotation_y: " << fixed << std::setprecision(7) << transform.rotation.y << std::endl;
+	std::cout << "rotation_z: " << fixed << std::setprecision(7) << transform.rotation.z << std::endl;
+	std::cout << "rotation_w: " << fixed << std::setprecision(7) << transform.rotation.w << std::endl;
+
+	ofstream ofs(file_name_, ios::app);
+	if(ofs) {
+		ofs << "    - {" <<std::endl;
+		ofs << "        location_name: \"" << location_name << "\","<<std::endl;
+		ofs << "        frame_id: \"map\","<<std::endl;
+		ofs << "        translation_x: " << fixed << std::setprecision(7) << transform.translation.x << "," << std::endl;
+		ofs << "        translation_y: " << fixed << std::setprecision(7) << transform.translation.y << "," << std::endl;
+		ofs << "        translation_z: " << fixed << std::setprecision(7) << transform.translation.z << "," << std::endl;
+		ofs << "        rotation_x: " << fixed << std::setprecision(7) << transform.rotation.x << "," << std::endl;
+		ofs << "        rotation_y: " << fixed << std::setprecision(7) << transform.rotation.y << "," << std::endl;
+		ofs << "        rotation_z: " << fixed << std::setprecision(7) << transform.rotation.z << "," << std::endl;
+		ofs << "        rotation_w: " << fixed << std::setprecision(7) << transform.rotation.w << "," << std::endl;
+		ofs << "    }" <<std::endl;
+		ofs << std::endl;
+		ofs.close();
+		std::cout <<  "「" << file_name_ << "」として保存完了。"  << std::endl;
+
+		geometry_msgs::PoseStamped pose;
+		pose.pose.position.x = transform.translation.x;
+		pose.pose.position.y = transform.translation.y;
+		pose.pose.position.z = transform.translation.z;
+		pose.pose.orientation.x = transform.rotation.x;
+		pose.pose.orientation.y = transform.rotation.y;
+		pose.pose.orientation.z = transform.rotation.z;
+		pose.pose.orientation.w = transform.rotation.w;
+		pose.header.frame_id = location_name;
+		pub_pose_.publish(pose);
+	} else {
+		ofs.close();
+		std::cout <<  file_name_ << "は作成できませんでした。"  << std::endl;
+		std::cout << "ファイルのパスを確認して下さい。" << std::endl;
+	}
+	return;
+}
+
+void CreateLocationFile::callbackMessage( const geometry_msgs::PoseStampedConstPtr &msg ) {
+	nav_goal_msg_ = msg->pose;
 }
 
 CreateLocationFile::CreateLocationFile( ) : nh_(), pnh_("~"), tfBuffer_(), tf_listener_(tfBuffer_) {
+	nh_.getParam("/use_robot", use_robot_);
+	if (!use_robot_) {
+		sub_msg_ = nh_.subscribe( "/move_base_simple/goal", 1, &CreateLocationFile::callbackMessage, this );
+	}
     pub_pose_ = nh_.advertise<geometry_msgs::PoseStamped>("/location_pose", 1);
 }
 
@@ -111,15 +133,20 @@ void CreateLocationFile::createLocationFile() {
 		ofs << "location_pose:" <<std::endl;
 	} else {
 		ofs.close();
-		std::cout <<  file_name_ << "　は作成できませんでした。ｍ(_ _;)ｍ"  << std::endl;
+		std::cout <<  file_name_ << "は作成できませんでした。"  << std::endl;
 		std::cout << "ファイルのパスを確認して下さい。" << std::endl;
 	}
 	while(ros::ok()) {
 		std::cout <<"========================================" << std::endl;
 		std::cout <<"[ 登録地点一覧 ]" << std::endl;
 		int i = 1;
-		for ( const auto& name : location_names ) std::cout << "    [ " << i++ << " ] : " << name << std::endl; 
-		std::cout <<  "\n[ ロボットの現在地点を登録 ] \n場所名を入力してください。「q」で終了。\nLocation Name : ";
+		for ( const auto& name : location_names ) std::cout << "    [ " << i++ << " ] : " << name << std::endl;
+		if (use_robot_) {
+			std::cout <<  "\n[ ロボットの現在地点を登録 ] \n場所名を入力してください。「q」で終了。\nLocation Name : ";
+		}
+		else {
+			std::cout <<  "\n[ クリックした地点を登録 ] \n場所名を入力してください。「q」で終了。\nLocation Name : ";
+		}
 
 		std::getline(std::cin, location_name);
 		if(location_name =="q") {
@@ -127,7 +154,8 @@ void CreateLocationFile::createLocationFile() {
 			ros::Duration(2).sleep();
 			exit(EXIT_SUCCESS);
 		}else {
-			std::cout << "\n現在地点を「" << location_name << "」で保存します。" << std::endl;
+			if (use_robot_) std::cout << "\n現在地点を「" << location_name << "」で保存します。" << std::endl;
+			else    std::cout << "\nクリックした地点を「" << location_name << "」で保存します。" << std::endl;
 			saveLocation( location_name );
 			location_names.push_back(location_name);
 		}
