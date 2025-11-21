5b5061277c705eaf839a0ef2c1521ba3c8a2ef04
r.jochi <131255788+Rjochi@users.noreply.github.com>
2025-11-22 01:43:47 +0900

    [FIX] IfCondition

--- sobits_slam/launch/active_slam.launch.py
+++ sobits_slam/launch/active_slam.launch.py
@@ -7,7 +7,7 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, Opaq
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-
+from launch.conditions import IfCondition
 
 def generate_launch_description():
     ########## Customizable parameters ##########
