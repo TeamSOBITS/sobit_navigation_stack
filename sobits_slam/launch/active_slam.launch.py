e402e6e5138bad59a7eb4b11331e9c379397ec96
Jiahao9 <simjiahao9@gmail.com>
2026-03-24 16:49:59 +0900

    [FIX] Correct conditional logic for robot base frame assignment in active_slam.launch.py

--- sobits_slam/launch/active_slam.launch.py
+++ sobits_slam/launch/active_slam.launch.py
@@ -104,7 +104,7 @@ def declare_param_value(context, *args, **kwargs):
     robot_name_value = LaunchConfiguration('robot_name').perform(context)
     if ("home" in robot_name_value):
         robot_base_frame = robot_name_value + "/base_footprint"
-    if ("pro" in robot_name_value):
+    elif ("pro" in robot_name_value):
         robot_base_frame = robot_name_value + "/lidar_laser" # "/base_footprint"
     elif ("edu" in robot_name_value):
         robot_base_frame = robot_name_value + "/lidar_laser" # "/base_footprint"
